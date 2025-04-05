254f9cf1f2226fb5cd8a0d37aa4fa842877d6586
MohammadWaseem6 <wa6865688@gmail.com>
2025-04-06 01:15:24 +0530

    day 1

--- hello.py
+++ hello.py
@@ -1,1 +1,46 @@
-print("Hello, World!")
+#strings
+first_name ="Bro"
+food= "Pizza"
+email="me@gmail.com"
+
+#Integers
+age = 25
+quantity =6
+
+#Float
+price= 10.77
+
+#boolean
+
+is_Stuents= True
+for_sale = False
+
+# if for_sale:
+#     print("This car is For Sale")
+# else:
+#      print("This car is Not Availabe")
+     
+#exercise
+user_name = "WASEEM AHAMD"
+Year=2024
+pi=3.14
+is_admin= True
+# print(f"my name is {user_name} ")
+# print(f"Year is {Year}")
+# print(f"Value of PI is :{pi} ")
+# print(f"am i admin : {is_admin}")
+
+#TypeCasting
+#str() , int() ,float(), bool()
+
+name = "Hifi"
+age = 25
+gpa=3.5
+is_Stuent =  True
+
+
+age = str(age) 
+gap = int(gpa)
+name = bool(name)
+
+
